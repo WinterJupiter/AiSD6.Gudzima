f70bd56b70e6fc669f576c93c99f0d2e08e22495
Jupiter <90478585+WinterJupiter@users.noreply.github.com>
2022-04-26 18:04:14 +0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,26 +4,6 @@
 
 using namespace std;
 
-struct stats {
-    size_t comparison_count = 0;
-    size_t copy_count = 0;
-};
-
-stats Bubble(std::vector<int>& data)
-{
-
-}
-
-stats Shaker(std::vector<int>& data)
-{
-    
-}
-
-stats Heap(std::vector<int>& data)
-{
-    
-}
-
 int get_k()
 {
     while (true)
@@ -36,11 +16,74 @@ int get_k()
     }
 }
 
+vector<int> input(size_t n)
+{
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int value;
+        cin >> value;
+        v.push_back(value);
+    }
+    return v;
+}
+
+struct stats {
+    size_t comparison_count = 0;
+    size_t copy_count = 0;
+};
+
+void Print(vector<int> v)
+{
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i];
+    }
+    std::cout << std::endl;
+}
+
+stats Bubble(std::vector<int>& data)
+{
+    stats bubble;
+    for (size_t i = 0; i < data.size() - 1; i++)
+    {
+        for (size_t j = 0; j < data.size() - i - 1; j++)
+        {
+            if (data[j] > data[j + 1])
+            {
+                int tmp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = tmp;
+                bubble.comparison_count++;
+                bubble.copy_count++;
+            }
+        }
+    }
+    Print(data);
+    return bubble;
+}
+
+stats Shaker(std::vector<int>& data)
+{
+    stats shaker;
+    Print(data);
+    return shaker;
+}
+
+stats Heap(std::vector<int>& data)
+{
+    stats heap;
+    Print(data);
+    return heap;
+}
 
 int main()
 {
-    cout << "Input vector: ";
-
+    size_t n;
+    cout << "Let's work!\nVector size: ";
+    cin >> n;
+    vector<int> v = input(n);
+    vector<int> w = v;
     while (true)
     {
         system("cls");
@@ -48,15 +91,21 @@ int main()
         int m = get_k();
         if (m == 1)
         {
-
+            Bubble(w);
+            system("pause");
+            w = v;
         }
         if (m == 2)
         {
-
+            Shaker(w);
+            system("pause");
+            w = v;
         }
         if (m == 3)
         {
-
+            Heap(w);
+            system("pause");
+            w = v;
         }
         if (m == 4) break;
     }
